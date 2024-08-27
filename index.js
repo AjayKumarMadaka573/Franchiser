1ebebc49ff4a5fcd541f012366e056853632f8a3
AjayKumarMadaka <sanjiv12p@gmail.com>
2024-08-27 23:22:44 +0530

    main6 commit

--- index.js
+++ index.js
@@ -53,6 +53,6 @@ app.get('/TheFranchiser',async (req,res)=>{
 });
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   
